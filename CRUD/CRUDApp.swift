9362e72204417805307ede634bd22d150cbe5b72
Rabin <rabin@mobiledge.io>
2025-05-20 18:07:44 -0600

    Initial Commit

---
+++ CRUD/CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDApp.swift
+//  CRUD
+//
+//  Created by Rabin Joshi on 2025-05-20.
+//
+
+import SwiftUI
+
+@main
+struct CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CRUD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CRUD
+//
+//  Created by Rabin Joshi on 2025-05-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
